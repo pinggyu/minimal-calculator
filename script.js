fc617605f0e1dd108d95b9a8ef5d89c744c858ad
Ping Yu <pingyuuu@hotmail.com>
2022-08-04 11:54:55 -0400

    added logic for operations and results screen, chaining of operations

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// TO DO: chained operations
-// resutls screen: display current num typed by digit, result
-// operations screen: display first num after operator chosen + display operator + display second num after = is typed and display = 
-// limit num of digits displayed in operations screen
-// fix bug where second num is limited in terms of digits 
+// make this work on mobile
 
 // initialize namespace object
 const app = {};
 
-const MAX_DIGITS_DISPLAYED = 20;
+const MAX_DIGITS_DISPLAYED = 14;
 
 app.displayValue = '';
-app.firstNum = 0;
-app.secondNum = 0;
-app.operationResult = 0;
+app.firstNum = '';
+app.secondNum = '';
+app.operationResult = '';
 app.currentOperator = '';
 app.equalBtn = document.getElementById('calcCompute');
 app.resultDisplay = document.getElementById('resultScreen');
@@ -30,50 +26,68 @@ app.init = function() {
 app.checkModifier = function() {
     document.querySelectorAll('.calcModifier').forEach(btn => btn.addEventListener('click', function(e){
         let numModifier = btn.textContent;
-        console.log(btn.textContent);
         app.displayValue = app.modifier(numModifier,app.displayValue)
         app.displayOperation();
     }));    
 }
 
 app.modifier = function(modifier, num){
-    console.log(modifier);
     switch(modifier) {
         case '+/-':
-            return -num;
+            // converted to string since app.displayValue.length is used to determine whether current input overflows screen
+            return (-num).toString();
         case '%':
-            return num/100;
+            return (num/100).toString();
         case '.':
             return num + '.';
     }
 }
 
-app.operate = function(operator, num1, num2){
-    switch(operator) {         
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '×':
-            return num1 * num2;
-        case '÷':
-        if (num2 === 0){
-            alert('Division by zero is undefined!'); 
-            break;
-        } else {
-            return num1 / num2; 
-        };
-    }
+app.displayOperation = function(){
+    app.resultDisplay.textContent = '';
+    app.resultDisplay.textContent = `${app.displayValue}`;
+    app.operationDisplay.textContent = `${app.firstNum} ${app.currentOperator} ${app.secondNum}`
+}
+
+app.displayResult = function() {
+    app.resultDisplay.textContent = '';
+    app.resultDisplay.textContent = `${app.displayValue}`;
+    app.operationDisplay.textContent = `${app.firstNum} ${app.currentOperator} ${app.secondNum} =`
 }
 
 app.checkCompute = function() {
     app.equalBtn.addEventListener('click', function(e){
+        app.operationResult = 0;
         app.secondNum = parseFloat(app.displayValue);
         app.firstNum = parseFloat(app.firstNum);
-        app.operationResult = app.operate(app.currentOperator, app.firstNum, app.secondNum);
-        app.displayOperation();
+        app.operationResult = app.operate();
+        app.displayValue = app.operationResult;
         app.displayResult();
+        app.chainOperation();
     })
+}
+
+app.chainOperation = function() {
+    app.firstNum = app.operationResult;
+    app.secondNum = '';    
+}
+
+app.operate = function(){
+    switch(app.currentOperator) {         
+        case '+':
+            return app.firstNum + app.secondNum;
+        case '-':
+            return app.firstNum - app.secondNum;
+        case '×':
+            return app.firstNum * app.secondNum;
+        case '÷':
+        if (app.secondNum === 0){
+            alert('Division by zero is undefined!'); 
+            break;
+        } else {
+            return app.firstNum / app.secondNum; 
+        };
+    }
 }
 
 app.checkOperator = function(){
@@ -81,49 +95,33 @@ app.checkOperator = function(){
         app.currentOperator = btn.textContent;
         app.firstNum = app.displayValue;
         app.displayOperation();
+        app.displayValue = '';
     }));
 }
 
 app.getNums = function() {    
     document.querySelectorAll('.numBtn').forEach(btn => btn.addEventListener('click', function(e){
-        if (app.displayValue === app.firstNum){
-            app.displayValue = '';
-        }
-        if (app.displayValue.length < MAX_DIGITS_DISPLAYED) {
+        if (app.displayValue.length > MAX_DIGITS_DISPLAYED) {
+            alert('Max number of digits entered!');
+        } else {
             app.displayValue += btn.textContent;
             app.displayOperation();
-        } else {
-            alert('Max number of digits entered!');
         }
     })); 
 }
 
-app.displayOperation = function(){
+app.resetCalculator = function() {
+    app.operationDisplay.textContent = '';
     app.resultDisplay.textContent = '';
-    app.resultDisplay.textContent = `${app.displayValue}`;
-    if (app.currentOperator && !app.secondNum) {
-         app.operationDisplay.textContent = `${app.firstNum} ${app.currentOperator}`
-    } else if (app.currentOperator && app.secondNum) {
-        app.operationDisplay.textContent = `${app.firstNum} ${app.currentOperator} ${app.secondNum}`
-    }
-}
-
-app.displayResult = function(){
-    app.resultDisplay.textContent = '';
-    app.resultDisplay.textContent = `${app.operationResult}`;
+    app.displayValue = '';
+    app.firstNum = '';
+    app.secondNum = '';
+    app.operationResult = '';
+    app.currentOperator = '';    
 }
 
 app.checkClear = function() {
-    app.clearBtn.addEventListener('click', function(e){
-        app.operationDisplay.textContent = '';
-        app.resultDisplay.textContent = '';
-        app.displayValue = '';
-        app.firstNum = 0;
-        app.secondNum = 0;
-        app.operationResult = 0;
-        app.currentOperator = '';
-
-    })
+    app.clearBtn.addEventListener('click', app.resetCalculator)
 }
 
 app.init();
